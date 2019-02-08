0a141bc733b06c80ae4e2f4338ed66726a3430bf
umaepoch <umag@epochconsulting.in>
2019-02-08 14:36:08 +0530

    Nhance Update

--- nhance/nhance/doctype/stock_requisition/stock_requisition.js
+++ nhance/nhance/doctype/stock_requisition/stock_requisition.js
@@ -89,9 +89,6 @@ erpnext.buying.MaterialRequestController = erpnext.buying.BuyingController.exten
 					cur_frm.add_custom_button(__("Supplier Quotation"),
 					this.make_supplier_quotation, __("Make"));
 
-				if(doc.material_request_type === "Manufacture")
-					cur_frm.add_custom_button(__("Work Order"),
-					this.raise_work_orders(), __("Make"));
 
 				cur_frm.page.set_inner_btn_group_as_primary(__("Make"));
 
